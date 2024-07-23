0ebbaf40e8ac07d76bdfd4fbd7688b6a38771df5
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-07-23 11:16:06 -0300

    feat: ajuste classe Notification Service

--- src/main/java/com/julia/desafio_backend/service/NotificationService.java
+++ src/main/java/com/julia/desafio_backend/service/NotificationService.java
@@ -19,14 +19,6 @@ public class NotificationService {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-      /* ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
-
-       if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
-        System.out.println("Erro ao enviar notificação");
-        throw new Exception("Não foi possível acessar o serviço de notificação");
-        
-       } */ 
-
        System.out.println("Notificação enviada para o usuário");
     }
 
